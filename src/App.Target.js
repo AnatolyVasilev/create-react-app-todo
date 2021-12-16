59e63844384ecfa934e804f58deb7b4c5dc80ea2
anatoliivasilev <anatolii.s.vasilev@gmail.com>
2021-12-16 10:56:35 +0300

    first commit

---
+++ src/App.Target.js
@@ -0,0 +1,92 @@
+import "./App.css";
+import {useEffect, useState} from "react";
+import {TodoService} from "./TodoService";
+
+const service = new TodoService();
+
+function App() {
+
+    const [taskName, setTaskName] = useState('');
+    const onChangeTaskName = (e) => {
+        setTaskName(e.target.value);
+    };
+    const onSubmitTaskName = (e) => {
+        service.addTask(taskName);
+    };
+
+    const [tasks, setTasks] = useState([]);
+    useEffect(() => {
+        setTasks(tasks.concat(service.getData()));
+        const unsubscribe = service.subscribe(() => {
+            setTasks(tasks.concat(service.getData()));
+        });
+        return () => {
+            unsubscribe();
+        };
+    }, []);
+
+    const removeTask = (id) => {
+        return () => {
+            service.removeTask(id);
+        };
+    }
+
+    const finishTask = (id) => {
+        return () => {
+            service.setActive(id, false);
+        };
+    }
+
+    return <div className="app-container d-flex align-items-center justify-content-center flex-column">
+        <h3>Todo App</h3>
+        <div className="d-flex align-items-center mb-3">
+            <div className="form-group mr-3 mb-0">
+                <input
+                    value={taskName}
+                    onChange={onChangeTaskName}
+                    type="text"
+                    className="form-control"
+                    placeholder="Enter a task here"
+                />
+            </div>
+            <button
+                type="button"
+                className="btn btn-primary mr-3"
+                onClick={onSubmitTaskName}
+            >
+                Save
+            </button>
+        </div>
+        <div className="table-wrapper">
+            <table className="table table-hover table-bordered">
+                <thead>
+                <tr>
+                    <th>No.</th>
+                    <th>Todo item</th>
+                    <th>status</th>
+                    <th>Actions</th>
+                </tr>
+                </thead>
+                <tbody>
+                {tasks.map(task => <tr key={task.id} className="{{ task.status ? 'table-success' : 'table-light' }}">
+                    <td>{task.id}</td>
+                    <td className={!task.active ? 'complete' : 'task'}>
+                        {task.name}
+                    </td>
+                    <td>{!task.active ? "Completed" : "In progress"}</td>
+                    <td>
+                        <button className="btn btn-danger" onClick={removeTask(task.id)}>
+                            Delete
+                        </button>
+                        <button className="btn btn-success" onClick={finishTask(task.id)}>
+                            Finished
+                        </button>
+                    </td>
+                </tr>)}
+                </tbody>
+            </table>
+        </div>
+    </div>;
+}
+
+export default App;
--- src/App.js
+++ src/App.js
@@ -1,24 +1,87 @@
-import logo from './logo.svg';
 import './App.css';
+// import {useEffect, useState} from "react";
+// import {TodoService} from "./TodoService";
+
+// const service = new TodoService();
 
 function App() {
+
+    // ADD TASK FORM
+    // const [taskName, setTaskName] = useState('');
+    // const onChangeTaskName = (e) => {
+    //     setTaskName(e.target.value);
+    // };
+    // const onSubmitTaskName = (e) => {
+    //     service.addTask(taskName);
+    // };
+
+    // TASKS LIST
+    // const [tasks, setTasks] = useState([]);
+    // useEffect(() => {
+    //     setTasks(tasks.concat(service.getData()));
+    //     const unsubscribe = service.subscribe(() => {
+    //         setTasks(tasks.concat(service.getData()));
+    //     });
+    //     return () => {
+    //         unsubscribe();
+    //     };
+    // }, []);
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      Hello World!
     </div>
+    // <div className="app-container d-flex align-items-center justify-content-center flex-column">
+    //     <h3>Todo App</h3>
+    //     <div className="d-flex align-items-center mb-3">
+    //         <div className="form-group mr-3 mb-0">
+    //             <input
+    //                 value={taskName}
+    //                 onChange={onChangeTaskName}
+    //                 type="text"
+    //                 className="form-control"
+    //                 placeholder="Enter a task here"
+    //             />
+    //         </div>
+    //         <button
+    //             type="button"
+    //             className="btn btn-primary mr-3"
+    //             onClick={onSubmitTaskName}
+    //         >
+    //             Save
+    //         </button>
+    //     </div>
+    //     <div className="table-wrapper">
+    //         <table className="table table-hover table-bordered">
+    //             <thead>
+    //             <tr>
+    //                 <th>No.</th>
+    //                 <th>Todo item</th>
+    //                 <th>status</th>
+    //                 <th>Actions</th>
+    //             </tr>
+    //             </thead>
+    //             <tbody>
+    //             {tasks.map(task => <tr key={task.id} className="{{ task.status ? 'table-success' : 'table-light' }}">
+    //                 <td>{task.id}</td>
+    //                 <td className={!task.active ? 'complete' : 'task'}>
+    //                     {task.name}
+    //                 </td>
+    //                 <td>{!task.active ? "Completed" : "In progress"}</td>
+    //                 <td>
+    //                     <button className="btn btn-danger" onClick={removeTask(task.id)}>
+    //                         Delete
+    //                     </button>
+    //                     <button className="btn btn-success" onClick={finishTask(task.id)}>
+    //                         Finished
+    //                     </button>
+    //                 </td>
+    //             </tr>)}
+    //             </tbody>
+    //         </table>
+    //     </div>
+    // </div>
   );
 }
 
